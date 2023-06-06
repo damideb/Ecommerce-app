abb2c5fb964f873a9c7b6a20a8e163f431a70602
Damilola Adebowale <adebzdamzy@gmail.com>
2023-06-06 17:55:44 +0100

    neww

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import {BrowserRouter as Router} from "react-router-dom"
 
+import {ContextProvider} from "./Context"
+import App from "./App"
 
- ReactDOM.render(<App/>, document.getElementById('root'));
-
-
-
-
+ReactDOM.render(
+    <ContextProvider>
+        <Router>
+            <App />
+        </Router>
+    </ContextProvider>, 
+    document.getElementById("root")
+)
