f934e42c598797b05326ca7d515bfad204069ffe
Damilola Adebowale <adebzdamzy@gmail.com>
2023-06-18 22:30:18 +0100

    hook

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -3,9 +3,18 @@ import {useState, useEffect, useRef} from "react"
 function useHover(){
 
 const[hovered, setHovered] = useState(false)
+const ref = useRef(null)
+
+function enter(){
+    setHovered(true)
+}
+function leave(){
+    setHovered(false)
+}
 
 useEffect(()=>{
     
+    ref.current.addEventListener(onmouseenter, enter)
 })
 
 }
