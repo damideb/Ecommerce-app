1e80766d998d721ce6b47c7ad335f73d1701d380
Damilola Adebowale <adebzdamzy@gmail.com>
2023-06-22 21:01:35 +0100

    neww

--- src/Item.js
+++ src/Item.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import {Context} from "./Context"
 
-function ItemDescription(){
- 
+
+function ItemDescription(props){
+    const navigate = useNavigate()
+    const{allPhotos} = useContext(Context)
+    const{itemId} = useParams()
+    const bagItem = allPhotos.find(item=> item.id===itemId)
+
+    function handleClick(){
+navigate('/')
+    }
     return(
         <div>
-            <p>Item Description</p>
+            <h3>Product Information</h3>
+            <p onClick={handleClick}><span> <i className="ri-arrow-go-back-line" ></i></span>Go back to homePage</p>
+           <img  src={bagItem.url}  alt='' className="bagItem"/>
+           <p>${bagItem.price}</p>
+           <button>Add to Cart</button>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,9 @@ function Header() {
     const cartClassName = cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
     return (
         <header>
-            <Link to="/"><h2>Pic Some</h2></Link>
+            <Link to="/"><h2>Home</h2></Link>
             <div className='cart-sign'>
-            <h4>Cart </h4>
+           <Link to='/cart'><h4>Cart </h4></Link> 
                 <Link to="/cart">  <i className={`${cartClassName} ri-fw ri-2x`}></i></Link>
             </div>
             
