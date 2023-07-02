fa2d4ff8bac577a383f5284b654c4e086eb82041
Damilola Adebowale <adebzdamzy@gmail.com>
2023-07-02 21:38:30 +0100

    neww

--- src/frontPage.js
+++ src/frontPage.js
@@ -10,7 +10,13 @@ function frontpage(){
         <h2>Discover more</h2>
     </nav>
     <div class="buyNowText"><i >buy now and get 30% off your next purchase!!!</i></div>
-   
+    <main class="homepage">
+        <div >
+            <h2>Made with love. Designed just for YOU</h2>
+            <p>finding the perfect accessories to match your outfit? try us out and get product shipped right to your door</p>
+<button>Explore our Range</button>
+        </div>
+    </main>
         </div>
     )
 }
