c8990e04bb6d38b0e9cedb8b27ab4f8a74e6f59f
Damilola Adebowale <adebzdamzy@gmail.com>
2023-07-02 21:37:57 +0100

    new commit

--- src/frontPage.js
+++ src/frontPage.js
@@ -4,7 +4,13 @@ function frontpage(){
 
     return(
         <div>
-
+  <nav class="head">
+        <h2>Home</h2>
+        <h2>About us</h2>
+        <h2>Discover more</h2>
+    </nav>
+    <div class="buyNowText"><i >buy now and get 30% off your next purchase!!!</i></div>
+   
         </div>
     )
 }
