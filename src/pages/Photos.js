9ca1614554b16f5ccee8e0fe8f64a73bac5f1c2e
Damilola Adebowale <adebzdamzy@gmail.com>
2023-07-05 22:05:51 +0100

    slice

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,32 +1,66 @@
-import React, {useContext} from "react"
+import React, {useContext, useState} from "react"
 
 import Image from "../components/Image"
 import {Context} from "../Context"
 
 
 function Photos() {
+    
     const {allPhotos} = useContext(Context)
 
-    console.log(allPhotos)
-    
     const imageElements = allPhotos.map((img) => (
         <Image key={img.id} img={img} />
     ))
 
- 
 
+    const[startIndex, setStartIndex] = useState(0)
+    const[endIndex, setEndIndex] = useState(10)
+    
+  
+
+ function next(){
+    
+    if(imageElements.length > endIndex){
+      
+        setStartIndex(startIndex+10)
+        setEndIndex(endIndex+10)
+        window.scrollTo(0,0)
+
+       
+
+        
+    }
+   
+ }
+
+ function previous(){
+    if( startIndex > 0 ){
+        window.scrollTo(0,0)
+        setStartIndex(startIndex- 10)
+        setEndIndex(endIndex - 10)
+    
+    }
+   
+ }
    
 
     
     return (
         <div>
         <main className="photos">
-            {imageElements.slice(0,10)}
+            {imageElements.slice(startIndex,endIndex)}
         </main>
         <div className="direction">
-        <button className="previous">&laquo; Previous</button>
-        <button className="next"
+        <button className="previous" 
+        onClick={previous}
+        >&laquo; Previous</button>
+       
+            <button className="next"
+        onClick={next}
         > next &raquo;</button>
+        
+    
+        
         </div>
         </div>
     )
