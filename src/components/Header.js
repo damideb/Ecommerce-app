5de6850fd467c11948e78d81710f7efdf168af23
Damilola Adebowale <adebzdamzy@gmail.com>
2023-07-15 00:14:09 +0100

    new commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,7 @@
 import React, {useContext} from "react"
 import {Link} from "react-router-dom"
 import {Context} from "../Context"
-import styled from "styled-components"
 
-
-const NavbarLink =styled.a`
-color:white;
-font-weight: lighter
-`
 function Header() {
     const {cartItems} = useContext(Context)
 
@@ -17,8 +11,8 @@ function Header() {
 
 <nav className="head">
     <h1>Styless</h1>
-    <h3><NavbarLink href='/'> Home </NavbarLink></h3>
-    <h3><NavbarLink href='/items'> Shop </NavbarLink></h3>
+    <h3><Link to='/'> Home </Link></h3>
+    <h3><Link to='/items'> Shop </Link></h3>
         <h3>contact us</h3>
         <Link to="/cart">  <i className={`${cartClassName} ri-fw ri-2x`}></i> </Link><small className="cartNumber">{cartItems.length}</small>
     </nav>
